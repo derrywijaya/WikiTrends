834560e7dbad5e3b794be2af31c23ef92e39242e
maryannecosgrove <maryanne.cosgrove@yale.edu>
2017-07-31 15:50:24 -0400

    Iterate through the files in the directory sequentially

--- backend/makeDict.py
+++ backend/makeDict.py
@@ -16,7 +16,11 @@ def main():
   dayCount = {}
   # iterate through the files in directory
   # this is an issue -- this assumes that the files in the dictionary are in a certain order, but they are not
-  for filename in os.listdir(sys.argv[1]):
+  
+  #for filename in os.listdir(sys.argv[1]):
+  
+  #for filename in sorted(os.listdir(sys.argv[1])), key=os.path.getctime):
+  for filename in sorted(os.listdir(sys.argv[1]), key=lambda f: os.path.getctime("{}/{}".format(sys.argv[1], f))):
     if currHour != 23:
       currHour += 1
       f = open(sys.argv[1] + "/" + filename)
